d513b1bf08d445c31e9a90337e04e49e6fcb1149
OlenaDoroshenko <70623973+OlenaDoroshenko@users.noreply.github.com>
2020-12-13 10:48:14 +0200

    task 4 updated

--- js/task4.js
+++ js/task4.js
@@ -3,16 +3,20 @@
 // Если длина строки не превышает 40 символов, функция возвращает ее в исходном виде.
 // Если длина больше 40 символов, то функция обрезает строку до 40-ка символов и добавляет в конец строки троеточие '...', после чего возвращает укороченную версию.
 let message;
+const maxLength = 40;
 const formatString = function (string) {
-    const symbols = string.split(``);
-    if (symbols.length > 40) {
-        const symbolsToRemove = symbols.length - 40;
-        const removedSymbols = symbols.splice(40, symbolsToRemove, "...");
+    // const symbols = string.split(``);
+    // if (symbols.length > 40) {
+    //     const symbolsToRemove = symbols.length - 40;
+    //     const removedSymbols = symbols.splice(40, symbolsToRemove, "...");
             
-        return symbols.join(``); 
-    }
+    //     return symbols.join(``); 
+    // }
 
-    return string;
+    // return string;
+    return string.length <= maxLength
+    ? string
+    : string.slice(0,maxLength) + `...`;
 };
 
 /*
